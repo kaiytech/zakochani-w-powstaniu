c17bd0c13b12105d27575721f987cf097ef9c25d
Kajtek <kaitek666@gmail.com>
2018-10-16 14:16:34 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Zakochani w Powstaniu</title>
+  <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.">
+  <meta name="author" content="Kajetan Krawczyk">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+    
+
+
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
